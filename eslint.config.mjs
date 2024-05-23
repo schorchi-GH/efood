c97c97f2a58876389c3e8510518efd9882577885
Schorchi <schorchi@gmail.com>
2024-05-23 14:43:38 -0300

    feat: add new banner component

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,40 +1,41 @@
-import globals from "globals";
-import js from "@eslint/js";
-import ts from "@typescript-eslint/eslint-plugin";
-import parser from "@typescript-eslint/parser";
-import react from "eslint-plugin-react";
-import prettier from "eslint-config-prettier";
-import prettierPlugin from "eslint-plugin-prettier";
+import globals from 'globals'
+import js from '@eslint/js'
+import ts from '@typescript-eslint/eslint-plugin'
+import parser from '@typescript-eslint/parser'
+import react from 'eslint-plugin-react'
+import prettier from 'eslint-config-prettier'
+import prettierPlugin from 'eslint-plugin-prettier'
 
 export default [
-  {
-    languageOptions: {
-      parser: parser,
-      parserOptions: {
-        ecmaVersion: "latest",
-        sourceType: "module",
-        ecmaFeatures: {
-          jsx: true,
+    {
+        files: ['src/**/*.{js,jsx,ts,tsx}'],
+        languageOptions: {
+            parser: parser,
+            parserOptions: {
+                ecmaVersion: 'latest',
+                sourceType: 'module',
+                ecmaFeatures: {
+                    jsx: true
+                }
+            },
+            globals: globals.browser
         },
-      },
-      globals: globals.browser,
+        plugins: {
+            react,
+            '@typescript-eslint': ts,
+            prettier: prettierPlugin
+        },
+        rules: {
+            'prettier/prettier': 'error',
+            'react-hooks/rules-of-hooks': 'error',
+            'react-hooks/exhaustive-deps': 'warn',
+            'react/prop-types': 'off',
+            'react/react-in-jsx-scope': 'off',
+            '@typescript-eslint/explicit-module-boundary-types': 'off'
+        }
     },
-  },
-  js.configs.recommended,
-  ts.configs.recommended,
-  react.configs.recommended,
-  {
-    plugins: {
-      prettier: prettierPlugin,
-    },
-    rules: {
-      "prettier/prettier": "error",
-      "react-hooks/rules-of-hooks": "error",
-      "react-hooks/exhaustive-deps": "warn",
-      "react/prop-types": "off",
-      "react/react-in-jsx-scope": "off",
-      "@typescript-eslint/explicit-module-boundary-types": "off",
-    },
-  },
-  prettier,
-];
+    js.configs.recommended,
+    ts.configs.recommended,
+    react.configs.recommended,
+    prettier
+]
--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
 import React from 'react'
 import { GlobalCSS } from './styles'
+import Banner from './components/Banner'
 
 function App() {
     return (
         <>
             <GlobalCSS />
             <h1>efood</h1>
+            <Banner />
         </>
-    )
+    );
 }
 
 export default App
